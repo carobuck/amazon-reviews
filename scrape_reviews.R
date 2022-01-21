230b6c8bd83235d6c744a47202031222a9d3bfcf
carobuck <cbuck717@gmail.com>
2022-01-20 17:04:10 -0800

    avoid bot detection

--- scrape_reviews.R
+++ scrape_reviews.R
@@ -1,3 +1,6 @@
+# Helping Galen with scraping Amazon reviews + NLP/analysis of reviews
+library(tidyverse)
+
 scrape_amazon <- function(ASIN, page_num){
   
   url_reviews <- paste0("https://www.amazon.com/product-reviews/",ASIN,"/?pageNumber=",page_num)
@@ -26,6 +29,30 @@ scrape_amazon <- function(ASIN, page_num){
          page = page_num) %>% return()
 }
 
+# Avoid bot detection
+ASIN <- "B07C12MRNX" # Specify ASIN
+page_range <- 1:10 # Let's say we want to scrape pages 1 to 10
+
+# Create a table that scrambles page numbers using `sample()`
+# For randomising page reads!
+match_key <- tibble(n = page_range,
+                    key = sample(page_range,length(page_range)))
+
+lapply(page_range, function(i){
+  j <- match_key[match_key$n==i,]$key
+  
+  message("Getting page ",i, " of ",length(page_range), "; Actual: page ",j) # Progress bar
+  
+  Sys.sleep(3) # Take a three second break
+  
+  if((i %% 3) == 0){ # After every three scrapes... take another two second break
+    
+    message("Taking a break...") # Prints a 'taking a break' message on your console
+    
+    Sys.sleep(2) # Take an additional two second break
+  }
+  scrape_amazon(ASIN = ASIN, page_num = j) # Scrape
+}) -> output_list
 
 test <- scrape_amazon(ASIN = "B07C12MRNX", page_num = 5) 
   
